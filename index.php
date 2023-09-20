a11c2a92007c6ad0bc1dc32f897d22950d2787a6
riohrmwn <riohermawan700@gmail.com>
2023-09-20 14:40:26 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+    <?php
+
+    $warna = "Jingga";
+        //kondisi
+    if ($warna == "Jingga") {
+        //nilai jika benar
+        echo "Jingga";
+    } else {
+        //selain itu jka tidak benar
+        echo "hitam";
+    }
+    
+
+    //Operator Ternary
+    //(Kondisi) ? Nilai Jika Benar : Nilai Jika Salah;
+
+    ?>
+
---
+++ umur.php
@@ -0,0 +1,11 @@
+<?php
+    //Operator Ternary
+    //(Kondisi) ? Nilai Jika Benar : Nilai Jika Salah;
+
+    $umur = 30;
+    $status = "Jomblo";
+
+    echo ($umur <= 30 && $status == "Jomblo") ? "Masih Seger" : "agak layu";
+
+    
+    ?>
